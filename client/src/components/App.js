810f423985513e9761163044537d0f987dd9b99b
Matt Draghi <mtd5264@gmail.com>
2022-03-24 10:57:22 -0400

    Cleaned up directories for react

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import '../styles/App.css'
 
 function App() {
   const [count, setCount] = useState(0);
